6c71884066c015ce4162a439f11d594efd689822
bonchovylkov <bonchovylkov>
2018-07-06 02:47:52 +0300

    fix problems when adding new node, create basic get node by index, remix crashed.. when deploy with few nodes..

--- SemanticNet_v2.sol
+++ SemanticNet_v2.sol
@@ -1,6 +1,6 @@
 
 pragma solidity ^0.4.24;
-import "https://github.com/Arachnid/solidity-stringutils/strings.sol";
+import "github.com/Arachnid/solidity-stringutils/strings.sol";
 
 
 
@@ -8,6 +8,7 @@ contract BlockchainSemanticTerminologyNet {
     
       using strings for *;
       
+      event LogDebug(string gotTo);
       event AddingNode(address fromAddr, uint number,string fullName,bytes32 uniqueNumber);
         
     address private owner;
@@ -28,31 +29,90 @@ contract BlockchainSemanticTerminologyNet {
          
          nodeNumber++;
         
+         addNode(0,"Block",owner); //1
+         addNode(1,"Transaction",owner); //2
+         addNode(0,"Node",owner); //3
+         addNode(3,"Peer",owner);//4
+         addNode(0,"Miner",owner);//5
+         addNode(0,"Wallet",owner);//6
+    }
+    
+    //name,creator,resourcesJoined, childrenJoined
+    function getNodeJson(uint nodeIndex) view public returns (string,address,string,string){
+        require(index<nodeNumber, "Please provide existing index");
+        
+        bytes32[] storage pathToNode = flattenTree[nodeIndex];
+        Node  ourNode = mainNode;
+        uint  index = 1;
+
+        while(ourNode.number!= nodeIndex && index < pathToNode.length){
+            
+            bytes32 nextItem = pathToNode[index];
+            ourNode = ourNode.children[nextItem];
+            
+            index++;
+        }
+        
+        strings.slice memory resources;
+           for (uint i=0; i< ourNode.resourcesKeys.length; i++) {
+          
+           resources = resources
+           .concat(ourNode.resourcesKeys[i].toSlice()).toSlice()
+           .concat(",".toSlice()).toSlice();
+           
+        }
+        
+            strings.slice memory childrenItems = "".toSlice();
+           for (uint j=0; j< ourNode.cheldrenKeys.length; j++) {
+          
+           childrenItems = childrenItems
+           .concat(ourNode.children[ourNode.cheldrenKeys[j]].name.toSlice()).toSlice()
+           .concat(",".toSlice()).toSlice();
+        }
+        
+        return (ourNode.name, ourNode.creator,resources.toString(),childrenItems.toString());
+        
+        
+        // string[] memory resources;
+        //  for (uint i=0; i< ourNode.resourcesKeys.length; i++) {
+          
+        //   strings.slice memory resourceKeyValue 
+        //   = ourNode.resourcesKeys[i].toSlice().concat(":".toSlice()).toSlice()
+        //   .concat(ourNode..toSlice()).toSlice();
+        // }
+        
+        
+        
+        
     }
     
     
-    
-    function addNode(uint parentNumber, string _name, address _creator) public {
+    function addNode(uint parentNumber, string _name, address _creator) public returns (uint){
         
         
         
 
         
         bytes32[] storage pathToNode = flattenTree[parentNumber];
-        Node storage parentNode = mainNode;
+        Node  parentNode = mainNode;
         uint  index = 1;
-        
+        // emit LogDebug("Got to 51");
         //prepare string to be concatenated
         strings.slice memory uniqueIdentifierPath = parentNode.name.toSlice().concat("->".toSlice()).toSlice();
-        
-        while(parentNode.number!=parentNumber){
+
+        while(parentNode.number!=parentNumber && index < pathToNode.length){
+            
             bytes32 nextItem = pathToNode[index];
             parentNode = parentNode.children[nextItem];
+            
+            //BUG: Blockchain->->Web Wallet???
             uniqueIdentifierPath = uniqueIdentifierPath
                                   .concat(parentNode.name.toSlice()).toSlice()
                                   .concat("->".toSlice()).toSlice();
             index++;
         }
+        
+        // emit LogDebug("Got to 65");
         
         uniqueIdentifierPath = uniqueIdentifierPath.concat(_name.toSlice()).toSlice();
         string memory fullName = uniqueIdentifierPath.toString();
@@ -62,19 +122,26 @@ contract BlockchainSemanticTerminologyNet {
         newNode.creator = _creator;
         newNode.number = nodeNumber;
         newNode.uniqueIdentifier = keccak256(abi.encodePacked(fullName));
+        // emit LogDebug("Got to 75");
+        
+        //copy the path to the parent in the flatten array of the new Item: ???
+        for (uint i=0; i<pathToNode.length; i++) {
+          flattenTree[newNode.number].push(pathToNode[i]);
+        }
         
         //keeping the flatten array actual
         flattenTree[newNode.number].push(newNode.uniqueIdentifier);
         
+        
         nodeNumber++;   
         
+        parentNode.children[newNode.uniqueIdentifier] = newNode;
+        parentNode.cheldrenKeys.push(newNode.uniqueIdentifier);
+        
         emit AddingNode(_creator,newNode.number,fullName,newNode.uniqueIdentifier);
+        
+        return newNode.number;
     }
-    
-    // function findNodeByNumber()  private returns (Node) {
-    //     returns mainNode;
-    // }
-    
     
     
       enum ReseurceType {
@@ -106,6 +173,8 @@ contract BlockchainSemanticTerminologyNet {
       
         //ipfs hash of term resource
         mapping(string=>ReseurceType)  resourcesHashes;
+        
+        string[] resourcesKeys;
         
         address  creator ;
         //can be positive or nagative value

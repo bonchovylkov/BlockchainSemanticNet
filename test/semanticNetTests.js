3dc3422bf5baf00de70097cdbd4dbb9596ef8898
Boncho Valkov <bonchovylkov@abv.bg>
2018-07-09 09:56:58 +0300

    update the tests after changes on the contract

--- test/semanticNetTests.js
+++ test/semanticNetTests.js
@@ -17,9 +17,10 @@ contract('SemanticNet', function ([owner]) {
     //    ["Blockchain","0xfa8871e962875d078135f1c5b27b0f184ab6f4dff8641dd81032226ea0ae9e8c",
     //    "0x627306090abab3a6e1400e9345bc60c78a8bef57","0","0x0000000000000000000000000000000000000000000000000000000000000000","0"]
         assert.equal((await semanticWeb.mainNode())[0], "Blockchain");
-        assert.equal((await semanticWeb.mainNode())[1].length, 66);
-        assert.equal((await semanticWeb.mainNode())[2], owner);
-        assert.equal((await semanticWeb.mainNode())[3], 0);
+        assert.equal((await semanticWeb.mainNode())[1], owner);
+        assert.equal((await semanticWeb.mainNode())[2], 0); //initial votes
+       
+        assert.equal((await semanticWeb.mainNode())[4], 0); //first number
         
     });
 
@@ -30,8 +31,10 @@ contract('SemanticNet', function ([owner]) {
     it('flatten tree set', async  ()=> {
         
         //calling mapping from uint => []
-        var firstId = await semanticWeb.flattenTree.call(0,0);
-        assert.equal(firstId, (await semanticWeb.mainNode())[1]);
+        var firstId = await semanticWeb.flattenTree.call(0,0); // gets the flattenTree[1][1]
+        //console.log(firstId.toNumber());
+        
+        assert.equal(firstId.toNumber(), (await semanticWeb.mainNode())[4].toNumber());
 
     });
 
@@ -51,7 +54,7 @@ contract('SemanticNet', function ([owner]) {
        assert.equal("Block", data[0]);
        assert.equal(owner, data[1]);
       // assert.equal("Block", data[0]);
-       assert.equal("Transaction,", data[3]);
+      // assert.equal("Transaction,", data[3]);
         //await semanticWeb.addNode({parentNumber:3,_name:"test"});
         //assert.equal(currentCount++, (await semanticWeb.mainNode())[1]);
     });
